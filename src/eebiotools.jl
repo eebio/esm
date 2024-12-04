4841bb68d4e292d638b508bfc168e4d2c71af536
Micha <micha_claydon@yahoo.co.uk>
2024-12-04 18:29:48 +0000

    EOD

--- src/eebiotools.jl
+++ src/eebiotools.jl
@@ -1,6 +1,6 @@
 module eebiotools
 
-using CSV, DataFrames, FileIO, JSON, Comonicon, FCSFiles, Parameters, JSONTables, XLSX
+using CSV, DataFrames, FileIO, JSON, Comonicon, FCSFiles, Parameters, JSONTables, JSONSchema, XLSX
 include("./io/read.jl")
 include("./base/main.jl")
 export read_fcs, read_csv, read_metadata, asscMetadata,leave, main, read_excel, write_conv
