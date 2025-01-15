13896ced186190c8d6d6c008d255869a4e4299e5
Micha <micha_claydon@yahoo.co.uk>
2025-01-15 16:43:55 +0000

    Moslty fixed now. Functions to be added.

--- src/io/ESM_read.jl
+++ src/io/ESM_read.jl
@@ -7,6 +7,7 @@ end
 
 function sexp_to_nested_list(sexp::Any,es,trans_meta_map)
     if isa(sexp, Symbol)
+        
         if isdefined(eebiotools,sexp)
             return sexp
         end
@@ -31,13 +32,27 @@ function sexp_to_nested_list(sexp::Any,es,trans_meta_map)
             elseif Symbol(string(sexp.args[1])) in keys(trans_meta_map)
                 # print(string(sexp[2].value))
                 # regex_pattern=Regex(string(sexp.args[2].value))
-                return filter_col(eval(sexp_to_nested_list(trans_meta_map[Symbol(string(sexp.args[1]))],es,trans_meta_map)),[string(sexp.args[2].value)])#[:,names(filter(colname -> occursin(regex_pattern, string(colname)), names(trans_meta_map[sexp])))]
+                # print(sexp)
+                if  string(sexp.args[2].value) in es.groups.group
+                    return filter_col(eval(sexp_to_nested_list(trans_meta_map[Symbol(string(sexp.args[1]))],es,trans_meta_map)),find_group(es,string(sexp.args[2].value)))#[:,names(filter(colname -> occursin(regex_pattern, string(colname)), names(trans_meta_map[sexp])))]
+                else
+                    print(filter_col(eval(sexp_to_nested_list(trans_meta_map[Symbol(string(sexp.args[1]))],es,trans_meta_map)),[string(sexp.args[2].value)]))
+                    return remove_subcols(filter_col(eval(sexp_to_nested_list(trans_meta_map[Symbol(string(sexp.args[1]))],es,trans_meta_map)),[string(sexp.args[2].value)]),sexp.args[2].value)#[:,names(filter(colname -> occursin(regex_pattern, string(colname)), names(trans_meta_map[sexp])))]
+                end
+                # elseif string(sexp.args[2].value) in es.groups.group
+                #     print("hello")
+                #     return filter_col(sexp_to_nested_list(sexp.args[1],es,trans_meta_map),es.groups.group[string(sexp.args[2].value)])
             else
-                print("here")
-                return filter_col(form_df(filter_row(es,sexp.args[1])),sexp.args[2].value) #group_map[Symbol(string(sexp.args[1],".", sexp.args[2].value))]
+                # print(sexp)
+                if  string(sexp.args[2].value) in es.groups.group
+                    return filter_col(form_df(filter_row(es,sexp.args[1])),find_group(es,string(sexp.args[2].value))) #group_map[Symbol(string(sexp.args[1],".", sexp.args[2].value))]
+                else
+                    # print("here")
+                    return remove_subcols(filter_col(form_df(filter_row(es,sexp.args[1])),[sexp.args[2].value]),sexp.args[2].value) #group_map[Symbol(string(sexp.args[1],".", sexp.args[2].value))]
+                end
             end
-        # elseif sexp.head == :. 
-        #     print(sexp.args[1].args,"hi")
+            # elseif sexp.head == :. 
+            #     print(sexp.args[1].args,"hi")
         else
             for arg in sexp.args
                 push!(result, sexp_to_nested_list(arg,es,trans_meta_map))
@@ -60,16 +75,17 @@ end
 function prod_v(es,trans_meta_map)
     v_out=Dict()
     for i in keys(es.views)
-        # print(i)
+        print(i)
         result = []
         for j in es.views[i]["data"]
-            print(j)
+            # print(j)
             if Symbol(j) in keys(trans_meta_map)
+                # show(sexp_to_nested_list(trans_meta_map[Symbol(j)],es,trans_meta_map))
                 push!(result,eval(sexp_to_nested_list(trans_meta_map[Symbol(j)],es,trans_meta_map)))
             elseif Symbol(j) in es.groups.group
                 push!(result,form_df(filter_row(es,Symbol(j))))
             elseif j in es.samples.name
-                print("here")
+                # print("here")
                 push!(result,form_df(es.samples[es.samples.name .== j,:]))
             end
         end
@@ -108,6 +124,11 @@ end
 #     return 0
 # end
 
+function remove_subcols(df,sub)
+    rename!(s -> replace(s,Regex("."*string(sub))=>""),df)
+    return df
+end
+
 function filter_row(es,group)
     # samples[samples."$group" .== true]
     return es.samples[es.samples[!,group] .== true,:]
@@ -122,15 +143,33 @@ function form_df(df)
     return hcat([DataFrame(j.name=>[j.values;fill(missing,max_dat-length(j.values))]) for j in eachrow(df)]...)
 end
 
+function find_group(es,grn)
+    return es.groups[es.groups.group .== grn,:sample_IDs][1]
+end
 
 function read_esm(filen)
     ef = JSON.parsefile(filen)
+    max_len=0
+    for i in keys(ef["samples"])
+        for j in keys(ef["samples"][i]["values"])
+            k = length(ef["samples"][i]["values"][j])
+            if k>max_len
+                max_len = k
+            end
+        end
+    end
+    # k=DataFrame([(i, j, ef["samples"][i]["type"], [i in ef["groups"][k]["sample_IDs"] for k in keys(ef["groups"])]...,ef["samples"][i]["values"][j]...) for i in keys(ef["samples"]) for j in keys(ef["samples"][i]["values"])])#,["name","channel","type","values",[k for k in keys(ef["groups"])]...]))
     es = esm_zones(
         samples=DataFrame([(i, j, ef["samples"][i]["type"],ef["samples"][i]["values"][j], [i in ef["groups"][k]["sample_IDs"] for k in keys(ef["groups"])]...) for i in keys(ef["samples"]) for j in keys(ef["samples"][i]["values"])],["name","channel","type","values",[k for k in keys(ef["groups"])]...]),#DataFrame(ef["samples"]),
         groups=DataFrame([(i,ef["groups"][i]["sample_IDs"],ef["groups"][i]["metadata"],:(filter(row -> row.name in ef["groups"][i]["sample_IDs"], samples, view=true))) for i in keys(ef["groups"])],["group","sample_IDs","metadata","meta_select"]),
         transformations=ef["transformations"],
         views=ef["views"]
     )
+    # test=groupby(es.samples, [Symbol(k) for k in keys(ef["groups"])])
+    # print(test[(:sg1=true)])
+    # test=filter_row(es,"sg1")
+    # print(DataFrame(test[test.channel .== "flo",[:name,:values]]) ./ DataFrame(test[test.channel .== "OD",[:name,:values]]))
+    # show( test[test.channel .== "OD",:values])
     es.samples.name = string.(es.samples.name,".",es.samples.channel)
     
     # print(in.(es.samples.name,  Ref(ef["groups"]["sg1"]["sample_IDs"])))
@@ -153,7 +192,11 @@ function read_esm(filen)
     # print(trans_meta_map)
     # print(dump(trans_meta_map[:model]))
     # print(sexp_to_nested_list(trans_meta_map[:model],group_map,es.samples,trans_meta_map))
-    print(prod_v(es,trans_meta_map))
+    # eval(sexp_to_nested_list(trans_meta_map[:combine_samples_flo],es,trans_meta_map))
+    for i in values(prod_v(es,trans_meta_map))
+        show([x for x in names(i)])
+        @df i display(plot(i))
+    end
     # print(trans_meta_map)
 
     # print(x)
@@ -165,12 +208,19 @@ mean(df::DataFrame) = return reduce(+, eachcol(df)) ./ ncol(df)
 
 hcat(x...) = return hcat(x)
 
+vcat(x...) = return vcat(x)
+
+groupby_flo(df::DataFrame) = return 
+
 function density_gate()
+end
+
+function process_pr(samples,blank,odchannel, flochannel)
 
 end
 
+### Needs standards curve creation, 
 function bead_calibrate()
-    
 end
 
 function model(x...)
