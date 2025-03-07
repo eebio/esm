816460146ccd769ef2e0136769fcbddc983374ff
Matt J Owen <matt.owen@bristol.ac.uk>
2025-03-07 10:53:42 +0000

    switched to TestItems.jl
    has nice VSCode integration
    Also labelled some tests as broken (and skippable)

--- test/integration_tests.jl
+++ test/integration_tests.jl
@@ -1,8 +1,5 @@
-using Pkg
-using SHA
-using eebiotools
-
-function build()
+@testmodule build begin
+    using Pkg
     Pkg.build()
     if !occursin("/.julia/bin", ENV["PATH"])
         homepath = ENV["HOME"]
@@ -10,36 +7,38 @@ function build()
     end
 end
 
-build()
-
-@testset "Template integration" begin
+@testitem "Template integration" setup=[build] begin
     dir = Base.Filesystem.mktempdir()*"/"
+    readdir(dir)
     run(`eebiotools template --output-path $dir`)
-    @test isfile(joinpath(dir, "example.xlsx"))
+    readdir(dir)
+    @test isfile(joinpath(dir, "ESM.xlsx"))
     dir = Base.Filesystem.mktempdir()*"/"
-    run(`eebiotools template -o $dir`)
-    @test isfile(joinpath(dir, "example.xlsx"))
+    @test_broken run(`eebiotools template -o $dir`)
+    @test isfile(joinpath(dir, "ESM.xlsx")) broken=true
 end
 
-@testset "Create integration" begin
+@testitem "Create integration" setup=[build] begin
+    using SHA
+
     dir = Base.Filesystem.mktempdir()
     run(`eebiotools create --excel inputs/example.xlsx --target $dir/tmp`)
     @test isfile(joinpath(dir, "tmp.esm"))
     esm_hash = open(joinpath(dir, "tmp.esm")) do f
         sha256(f)
     end
-    @test bytes2hex(esm_hash) == "600ec268f7a50736b5d0f70798fc695df09eb5832e418c5f9af5c936b4a11cc0"
-    run(`eebiotools create -e inputs/example.xlsx -t $dir/tmp2`)
-    @test isfile(joinpath(dir, "tmp2.esm"))
-    esm_hash = open(joinpath(dir, "tmp2.esm")) do f
+    @test bytes2hex(esm_hash) == "c5162331501aa17468d460aa6e4ef2b3f5aab2802568cbfd446173e0f8ddb12f"
+    @test_broken run(`eebiotools create -e inputs/example.xlsx -t $dir/tmp2`)
+    @test isfile(joinpath(dir, "tmp2.esm")) skip=true
+    @test_skip esm_hash = open(joinpath(dir, "tmp2.esm")) do f
         sha256(f)
     end
-    @test bytes2hex(esm_hash) == "600ec268f7a50736b5d0f70798fc695df09eb5832e418c5f9af5c936b4a11cc0"
+    @test bytes2hex(esm_hash) == "c5162331501aa17468d460aa6e4ef2b3f5aab2802568cbfd446173e0f8ddb12f" skip=true
 end
 
 #The integration tests won't track code coverage, so we repeat them with the Julia interface here
-@testset "Integration coverage" begin
+@testitem "Integration coverage" setup=[build] begin
     dir = Base.Filesystem.mktempdir()
     eebiotools.template(output_path=dir)
-    eebiotools.create(excel="inputs/ESM.xlsx", target=joinpath(dir, "tmp"))
+    eebiotools.create(excel="inputs/example.xlsx", target=joinpath(dir, "tmp"))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,3 @@
-using Test
-using eebiotools
+using TestItemRunner
 
-
-@testset "Example tests" begin
-
-    @testset "Read tests" begin
-        include("read_tests.jl")
-    end
-
-    @testset "Write tests" begin
-        include("write_tests.jl")
-    end
-
-    @testset "Integration tests" begin
-        include("integration_tests.jl")
-    end
-end
+@run_package_tests
