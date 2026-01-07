f90ff13919e9d0c24930cb41b6e18d982f8fc969
Matt J Owen <matt.owen@bristol.ac.uk>
2026-01-07 10:41:53 +0000

    fix aqua tests on pre/alpha
    slow precompile triggers the test failure (we don't have any persistent tasks)

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -1,5 +1,7 @@
 @testitem "Aqua" begin
     println("Aqua")
     using Aqua
-    Aqua.test_all(ESM)
+    Aqua.test_all(ESM; persistent_tasks = false)
+    # Covers for slow precomp for alpha builds of Julia
+    Aqua.test_persistent_tasks(ESM; tmax = 60)
 end
