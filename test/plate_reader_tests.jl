119b4c91eb7ae50f803f967eaac0de0e59c57c2b
Matt J Owen <matt.owen@bristol.ac.uk>
2026-05-14 10:41:03 +0100

    tests for growth rate plots new names

--- test/plate_reader_tests.jl
+++ test/plate_reader_tests.jl
@@ -532,8 +532,25 @@ end
         str = replace(str, "┌ Info: Saving growth curve plots to temporary directory: " => "")
         str = split(str, "\n")[1]
         @test isdir(str)
-        @test isfile(joinpath(str, "growth_curve_$(typeof(method))_A.png"))
-        @test isfile(joinpath(str, "growth_curve_$(typeof(method))_B.png"))
+        if hasproperty(method, :method)
+            extra = method.method
+        elseif hasproperty(method, :type)
+            extra = method.type
+        elseif hasproperty(method, :alg)
+            extra = method.alg
+        elseif hasproperty(method, :name)
+            extra = method.name
+        else
+            extra = ""
+        end
+        if extra != ""
+            @show readdir(str)
+            @test isfile(joinpath(str, "growth_curve_$(typeof(method))_$(extra)_A.png"))
+            @test isfile(joinpath(str, "growth_curve_$(typeof(method))_$(extra)_B.png"))
+        else
+            @test isfile(joinpath(str, "growth_curve_$(typeof(method))_A.png"))
+            @test isfile(joinpath(str, "growth_curve_$(typeof(method))_B.png"))
+        end
     end
 
     for func in (time_to_max_growth, lag_time, od_at_max_growth, max_od)
@@ -544,19 +561,16 @@ end
         @show str = String(take!(io))
         str = replace(str, "┌ Info: Saving growth curve plots to temporary directory: " => "")
         str = split(str, "\n")[1]
-        @show str
-        @test isdir(str)
-        @show readdir(str)
-        @test isfile(joinpath(str, "growth_curve_ESM.FiniteDiff_A.png"))
-        @test isfile(joinpath(str, "growth_curve_ESM.FiniteDiff_B.png"))
+        @test isfile(joinpath(str, "growth_curve_ESM.FiniteDiff_central_A.png"))
+        @test isfile(joinpath(str, "growth_curve_ESM.FiniteDiff_central_B.png"))
     end
 
     # Check user-specified directory
     custom_dir = mktempdir()
     growth_rate(od_df, time_col, FiniteDiff(); plot_directory = custom_dir)
     @test isdir(custom_dir)
-    @test isfile(joinpath(custom_dir, "growth_curve_ESM.FiniteDiff_A.png"))
-    @test isfile(joinpath(custom_dir, "growth_curve_ESM.FiniteDiff_B.png"))
+    @test isfile(joinpath(custom_dir, "growth_curve_ESM.FiniteDiff_central_A.png"))
+    @test isfile(joinpath(custom_dir, "growth_curve_ESM.FiniteDiff_central_B.png"))
 end
 
 @testitem "calibrate" begin
